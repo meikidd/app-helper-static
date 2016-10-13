20d7f99a343b7e9dc52c96b128e865ef9d09d8d5
meikidd <meikidd@gmail.com>
2016-10-14 00:30:27 +0800

    update: 将 px 单位转换成 rem 单位

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const pxtorem = require('postcss-pxtorem');
 
 module.exports = function (webpackConfig) {
   webpackConfig.output.path = path.resolve(__dirname, './build');
@@ -10,6 +11,13 @@ module.exports = function (webpackConfig) {
     libraryName: 'antd-mobile',
     style: true,  // if true, use less
   }]);
+
+  // 将 px 单位转换成 rem 单位
+  webpackConfig.postcss.push(pxtorem({
+    rootValue: 100,
+    propWhiteList: [],
+    selectorBlackList: [/^html$/, /^\.ant-/, /^\.github-/, /^\.gh-/],
+  }));
 
   Object.keys(webpackConfig.entry).forEach(name => {
     webpackConfig.plugins.push(new HtmlWebpackPlugin({
